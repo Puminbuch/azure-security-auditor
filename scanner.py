de1d949aadc75ec69312272e3a3ce285867c4813
qoyim <puminbuch2005@gmail.com>
2026-09-20 22:16:10 +0200

    VM-Prüfung hinzugefügt

--- scanner.py
+++ scanner.py
@@ -6,7 +6,13 @@ with open("testdaten.json") as f:
 print(daten)
 
 for eintrag in daten:
-    if eintrag["public_access"] or not eintrag["encryption_enabled"]:
-        print(eintrag["name"], "ist UNSICHER")
-    else:
-        print(eintrag["name"], "ist sicher")
+    if eintrag["type"] == "storage_account":
+        if eintrag["public_access"] or not eintrag["encryption_enabled"]:
+            print(eintrag["name"], "ist UNSICHER")
+        else:
+            print(eintrag["name"], "ist sicher")
+    elif eintrag["type"] == "vm":
+        if eintrag["ssh_open_to_all"] or eintrag["rdp_open_to_all"] or not eintrag["disk_encrypted"]:
+            print(eintrag["name"], "ist UNSICHER")
+        else:
+            print(eintrag["name"],"ist sicher")
