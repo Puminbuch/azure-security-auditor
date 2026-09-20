b434bd3543f265311e17c53cdf220978ff4060f5
qoyim <puminbuch2005@gmail.com>
2026-09-20 22:31:22 +0200

    Pruefungslogik in Funktionen ausgelagert

--- scanner.py
+++ scanner.py
@@ -1,4 +1,14 @@
 import json
+def prüfe_storage_account(eintrag):
+    if eintrag["public_access"] or not eintrag["encryption_enabled"]:
+        return True
+    else:
+        return False
+def prüfe_vm(eintrag):
+    if eintrag["ssh_open_to_all"] or eintrag["rdp_open_to_all"] or not eintrag["disk_encrypted"]:
+        return True
+    else:
+        return False
 
 with open("testdaten.json") as f:
     daten = json.load(f)
@@ -7,12 +17,12 @@ print(daten)
 
 for eintrag in daten:
     if eintrag["type"] == "storage_account":
-        if eintrag["public_access"] or not eintrag["encryption_enabled"]:
+        if prüfe_storage_account(eintrag):
             print(eintrag["name"], "ist UNSICHER")
         else:
             print(eintrag["name"], "ist sicher")
     elif eintrag["type"] == "vm":
-        if eintrag["ssh_open_to_all"] or eintrag["rdp_open_to_all"] or not eintrag["disk_encrypted"]:
+        if prüfe_vm(eintrag):
             print(eintrag["name"], "ist UNSICHER")
         else:
             print(eintrag["name"],"ist sicher")
